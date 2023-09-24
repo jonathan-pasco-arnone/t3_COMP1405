d63e4c4d52d9a298898ff5b042177959b9b54d87
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2023-09-24 18:47:19 +0000

    Testing connection

--- T3-Resources/validator.py
+++ T3-Resources/validator.py
@@ -18,7 +18,7 @@ def isvalid(string):
 
         # Checks if the character is each of the closing bracket types
         # If yes then it checks if the previous open bracket was the same type
-        # If yes again then it will take the opening bracket out of the stack
+        # If yes, again, then it will take the opening bracket out of the stack
         elif item == ")":
             if mystack.peek(bracket_openings) == "(":
                 mystack.pop(bracket_openings)
